aec8f99c534bf150f45d26ce67dbc2042cbaebe3
Chris McKnight <tenlongfingers10@gmail.com>
2022-09-18 16:17:30 -0600

    step 2 done

--- src/components/TopicBrowser/TopicBrowser.js
+++ src/components/TopicBrowser/TopicBrowser.js
@@ -1,7 +1,24 @@
 import React, { Component } from "react";
+
+//imported topics
+import EvenAndOdd from "../Topics/EvenAndOdd";
+import FilterObject from "../Topics/FilterObject";
+import FilterString from "../Topics/FilterString";
+import Palindrome from "../Topics/Palindrome";
+import Sum from "../Topics/Sum";
+
+//div container with all topics
 class TopicBrowser extends React.Component {
   render() {
-    return <p>Hello, World!</p>;
+    return (
+      <div>
+        <EvenAndOdd />
+        <FilterObject />
+        <FilterString />
+        <Palindrome />
+        <Sum />
+      </div>
+    );
   }
 }
 
