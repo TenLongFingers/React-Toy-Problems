efd3d5143626b977b245b48b71c836c2185aec98
Chris McKnight <tenlongfingers10@gmail.com>
2022-09-18 16:25:56 -0600

    Added outline to Even and Odd

--- src/components/Topics/EvenAndOdd.js
+++ src/components/Topics/EvenAndOdd.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 class EvenAndOdd extends Component {
   render() {
     return <p>Even and Odd</p>;
-    //split evens into one array and odds into another
+    //function that split evens into one array and odds into another (outside of the component? Can I add it at the bottom?)
     //h4
     //input
     //"split" button
