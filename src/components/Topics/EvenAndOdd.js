a09afe550b73cb4b77ea1a069a450028a2e2f179
Chris McKnight <tenlongfingers10@gmail.com>
2022-09-18 16:24:29 -0600

    upload

---
+++ src/components/Topics/EvenAndOdd.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+class EvenAndOdd extends Component {
+  render() {
+    return <p>Even and Odd</p>;
+    //split evens into one array and odds into another
+    //h4
+    //input
+    //"split" button
+    //two span elements for each array
+  }
+}
+
+export default EvenAndOdd;
