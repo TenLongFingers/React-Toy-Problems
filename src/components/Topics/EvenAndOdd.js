d3365eabb6882bbadee7796975af0a0948f2ea72
Chris McKnight <tenlongfingers10@gmail.com>
2022-09-18 16:40:49 -0600

    JSX added

--- src/components/Topics/EvenAndOdd.js
+++ src/components/Topics/EvenAndOdd.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
 class EvenAndOdd extends Component {
   render() {
-    return <p>Even and Odd</p>;
+    return (
+      <div className="puzzleBox evenAndOddPB">
+        <h4>Evens and Odds</h4>
+        <input className="inputLine" />
+        <button className="confirmationButton">Split</button>
+        <span className="resultsBox" />
+        <span className="resultsBox" />
+      </div>
+    );
     //function that split evens into one array and odds into another (outside of the component? Can I add it at the bottom?)
-    //h4
-    //input
-    //"split" button
-    //two span elements for each array
   }
 }
 
