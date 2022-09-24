24f51c5ac33e7a68aad0f6775affce93b3323365
Chris McKnight <tenlongfingers10@gmail.com>
2022-09-24 16:57:59 -0600

    handleChange

--- src/components/Topics/EvenAndOdd.js
+++ src/components/Topics/EvenAndOdd.js
@@ -1,10 +1,31 @@
 import React, { Component } from "react";
 class EvenAndOdd extends Component {
+  //constructor function here
+  constructor(props) {
+    super(props);
+    this.state = {
+      evenArray: [],
+      oddArray: [],
+      userInput: "",
+    };
+  }
+
+  handleChange = (val) => {
+    this.setState({ userInput: val });
+  };
+
+  sortEvenOdds = (number) => {
+    //grab the input, loop through one by one, and push it into evenArray or oddArray
+  };
+
   render() {
     return (
       <div className="puzzleBox evenAndOddPB">
         <h4>Evens and Odds</h4>
-        <input className="inputLine" />
+        <input
+          className="inputLine"
+          onChange={(e) => handleChange(e.target.value)}
+        />
         <button className="confirmationButton">Split</button>
         <span className="resultsBox" />
         <span className="resultsBox" />
