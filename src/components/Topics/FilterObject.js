a2c2207c2c9332dd6575d8b0355e08ece931c70a
Chris McKnight <tenlongfingers10@gmail.com>
2022-09-24 18:47:08 -0600

    set up filter object component

--- src/components/Topics/FilterObject.js
+++ src/components/Topics/FilterObject.js
@@ -1,7 +1,44 @@
 import React, { Component } from "react";
+
+//constructor
+
 class FilterObject extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      employees: [
+        {
+          name: "Jimmy Joe",
+          title: "Hack0r",
+          age: 12,
+        },
+        {
+          name: "Jeremy Schrader",
+          age: 24,
+          hairColor: "brown",
+        },
+        {
+          name: "Carly Armstrong",
+          title: "CEO",
+        },
+      ],
+      unFilteredArray: [],
+      userinput: "",
+      filteredArray: [],
+    };
+  }
+
+  //JSX
   render() {
-    return <p>Filter Object</p>;
+    return (
+      <div className="puzzleBox filterObject PB">
+        <h4>Filter Object</h4>
+        <span className="puzzleText"></span>
+        <input className="inputLine"></input>
+        <button className="confirmationButton"></button>
+        <span className="resultsBox filterObjectPB"></span>
+      </div>
+    );
   }
 }
 
